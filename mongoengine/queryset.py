8871c3d80f1fd7514e034d6aced462f84fffbd5b
Ross Lawley <ross.lawley@gmail.com>
2011-05-25 17:11:45 +0100

    Added insert method to the queryset to bulk inserts.
    Refs #43

--- mongoengine/queryset.py
+++ mongoengine/queryset.py
@@ -698,6 +698,46 @@ class QuerySet(object):
             result = None
         return result
 
+    def insert(self, doc_or_docs, load_bulk=True):
+        """bulk insert documents
+
+        :param docs_or_doc: a document or list of documents to be inserted
+        :param load_bulk (optional): If True returns the list of document instances
+
+        By default returns document instances, set ``load_bulk`` to False to
+        return just ``ObjectIds``
+
+        .. versionadded:: 0.5
+        """
+        from document import Document
+
+        docs = doc_or_docs
+        return_one = False
+        if isinstance(docs, Document) or issubclass(docs.__class__, Document):
+            return_one = True
+            docs = [docs]
+
+        raw = [doc.to_mongo() for doc in docs if isinstance(doc, self._document)]
+        if len(raw) != len(docs):
+            msg = "Some documents inserted aren't instances of %s" % str(self._document)
+            raise OperationError(msg)
+
+        raw = [doc.to_mongo() for doc in docs if doc.pk == None]
+        if len(raw) != len(docs):
+            msg = "Some documents have ObjectIds use doc.update() instead"
+            raise OperationError(msg)
+
+        ids = self._collection.insert(raw)
+
+        if not load_bulk:
+            return return_one and ids[0] or ids
+
+        documents = self.in_bulk(ids)
+        results = []
+        for obj_id in ids:
+            results.append(documents.get(obj_id))
+        return return_one and results[0] or results
+
     def with_id(self, object_id):
         """Retrieve the object matching the id provided.
 
